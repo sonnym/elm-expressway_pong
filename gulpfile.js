74c080e06463027dad02c71d1f479be8ca3ba9bb
sonnym <michaud.sonny@gmail.com>
2015-01-28 13:10:45 -0500

    prefer gulp-elm over custom task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,20 @@
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
 
 var gulp = require("gulp");
 var server = require("gulp-express");
 
-gulp.task("default", ["server"]);
+var elm = require("gulp-elm");
+
+gulp.task("default", ["server", "elm"]);
 
 gulp.task("server", function () {
   server.run({
     file: "lib/server.js"
   });
+});
 
-  gulp.watch("**/*.elm", function(_) {
-    exec("elm-make Pong/Client.elm --output public/pong.js", function(err, stdout, stderr) {
-      if (err) {
-        console.log(err);
-      }
-
-      if (stdout) {
-        console.log(stdout.toString());
-      }
-
-      if (stderr) {
-        console.log(stderr.toString());
-      }
-    });
-  });
+gulp.task("elm", function() {
+  return gulp.src("Pong/Client.elm")
+    .pipe(elm())
+    .pipe(gulp.dest("public"));
 });
